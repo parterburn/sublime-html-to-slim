66495bd5b8c598cd68882ecf8d2f58891535eb65
Paul Arterburn <paul@brandfolder.com>
2015-02-20 13:18:08 -0700

    Working version!

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# HTML to Slim plugin for Sublime Text 3
+# HTML to slim plugin for Sublime Text 3
 
-Converts selection content from HTML to SLIM using http://html2slim.herokuapp.com API
+Converts files, selection and clipboard content from HTML to slim using http://html2slim.raving.systems/html2slim.json API
+
+## Installation
 
 ### Git installation
 
 Clone the repository in your Sublime Text "Packages" directory:
 
-    git clone https://github.com/parterburn/sublime-html-to-slim.git "HTML to Slim"
+    git clone https://github.com/parterburn/sublime-html-to-slim.git "HTML2Slim"
 
 The "Packages" directory is located at:
 
@@ -22,7 +24,8 @@ The "Packages" directory is located at:
 
         %APPDATA%/Sublime Text 3/Packages/
 
-
 ### In Command Palette:
 
+* **HTML2Slim: Convert file**
 * **HTML2Slim: Convert selection**
+* **HTML2Slim: Convert clipboard content**
--- html2slim.py
+++ html2slim.py
@@ -1,5 +1,5 @@
 import urllib.request
-import urllib2
+import urllib.parse
 import json
 import sublime, sublime_plugin
 
@@ -34,18 +34,6 @@ class HtmlToSlimFromSelectionCommand(sublime_plugin.TextCommand):
 	def is_enabled(self):
 		return True #return self.view.file_name().endswith(".slim")
 
-class HtmlToSlimFromOtherSelectionCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		for region in self.view.sel():
-			if not region.empty():
-				html = self.view.substr(region)
-				slim = HTHTools.post_html_return_slim_other(html)
-				if slim != None:
-					self.view.replace(edit, region, slim)
-
-	def is_enabled(self):
-		return True #return self.view.file_name().endswith(".slim")		
-
 class HtmlToSlimFromClipboardCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		html = sublime.get_clipboard()
@@ -61,28 +49,12 @@ class HTHTools:
 	@classmethod
 	def post_html_return_slim(self, html):
 		host = 'http://html2slim.raving.systems/html2slim.json'
-		data = { 'source' : html }
-		data_json = json.dumps(data)
-		data_json = data_json.encode('utf-8')
-		req = urllib.request.Request(host, data_json, {'content-type': 'application/json'})
+		data = urllib.parse.urlencode({'source': html}).encode('utf-8')
+		req = urllib.request.Request(host, data, {})
 		response_stream = urllib.request.urlopen(req)
 		result = json.loads(response_stream.read().decode("utf-8"))
 
 		if result["result"]:
 			return result["result"]
-		else:
-			return None
-
-	def post_html_return_slim_other(self, html):
-		url = 'http://html2slim.raving.systems/html2slim.json'
-		data = { 'source' : html }
-		data_json = data.encode('utf-8')
-		data_json = urllib.urlencode(data_json)
-		req = urllib2.Request(url, data_json)
-		response = urllib2.urlopen(req)
-		result = json.loads(response.read().decode("utf-8"))
-
-		if result['result']:
-			return result['result']
 		else:
 			return None
